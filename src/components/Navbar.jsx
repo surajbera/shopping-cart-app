d018e932c1575ab74925239befb86706439e523b
suraj.be <suraj.be@media.net>
2023-01-20 10:57:02 +0530

    did some changes on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,27 +4,37 @@ import '../styles/navbar.scss'
 const Navbar = () => {
   return (
     <nav className='navbar'>
-      <NavLink to='/' end>
-        <i className='bi bi-shop-window' />
-      </NavLink>
+      <div className='container'>
+        <div className='logo'>
+          <NavLink to='/' end>
+            <i className='bi bi-shop-window' />
+          </NavLink>
+        </div>
 
-      <NavLink
-        to='/cart'
-        title='cart'
-        className={({ isActive }) => `${isActive && 'selected'}`}
-      >
-        <i className='bi bi-cart3' />
-        <sup className='cart-number'>7</sup>
-      </NavLink>
+        <div className='spacer'></div>
 
-      <NavLink
-        to='/'
-        className={({ isActive }) => (isActive ? 'selected' : '')}
-        title='products'
-        end
-      >
-        <i className='bi bi-grid' />
-      </NavLink>
+        <div className='products-wrap'>
+          <NavLink
+            to='/'
+            className={({ isActive }) => (isActive ? 'selected' : '')}
+            title='products'
+            end
+          >
+            <i className='bi bi-grid' />
+          </NavLink>
+        </div>
+
+        <div className='cart-wrapper'>
+          <NavLink
+            to='/cart'
+            title='cart'
+            className={({ isActive }) => `${isActive && 'selected'}`}
+          >
+            <i className='bi bi-cart3' />
+            <sup className='cart-number'>7</sup>
+          </NavLink>
+        </div>
+      </div>
     </nav>
   )
 }
